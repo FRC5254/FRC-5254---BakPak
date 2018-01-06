5f41fca171b8c89dbc8df534e97eeb8c652d1ae5
RyanSciarabba <rhsciarabba@gmail.com>
2018-01-06 09:48:19 -0600

    Added all the default files

---
+++ src/org/usfirst/frc/team5254/robot/OI.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team5254.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class OI {
+	
+	// Defining controllers
+	
+	public Joystick driver = new Joystick(RobotMap.DRIVER_JOYSTICK);
+	public Joystick operator = new Joystick(RobotMap.OPERATOR_JOYSTICK);
+
+	@SuppressWarnings("unused")
+	
+	public OI() {
+		
+		// Defining driver buttons
+		
+		Button DriverButtonA = new JoystickButton(driver, 1);
+		Button DriverButtonB = new JoystickButton(driver, 2);
+		Button DriverButtonX = new JoystickButton(driver, 3);
+		Button DriverButtonY = new JoystickButton(driver, 4);
+		Button DriverButtonBumperLeft = new JoystickButton(driver, 5);
+		Button DriverButtonBumperRight = new JoystickButton(driver, 6);
+		Button DriverButtonBack = new JoystickButton(driver, 7);
+		Button DriverButtonStart = new JoystickButton(driver, 8);
+		Button DriverButtonLeftJoystickPress = new JoystickButton(driver, 9);
+		Button DriverButtonRightJoystickPress = new JoystickButton(driver, 10);
+
+		// Defining operator buttons
+		
+		Button OperatorButtonA = new JoystickButton(operator, 1);
+		Button OperatorButtonB = new JoystickButton(operator, 2);
+		Button OperatorButtonX = new JoystickButton(operator, 3);
+		Button OperatorButtonY = new JoystickButton(operator, 4);
+		Button OperatorButtonBumperLeft = new JoystickButton(operator, 5);
+		Button OperatorButtonBumperRight = new JoystickButton(operator, 6);
+		Button OperatorButtonBack = new JoystickButton(operator, 7);
+		Button OperatorButtonStart = new JoystickButton(operator, 8);
+		Button OperatorButtonLeftJoystickPress = new JoystickButton(operator, 9);
+		Button OperatorButtonRightJoystickPress = new JoystickButton(operator, 10);
+
+	}
+}
---
+++ src/org/usfirst/frc/team5254/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team5254.robot;
+
+public class RobotMap {
+
+	// Solenoids
+	
+	// Sensors
+	
+	// Gyro
+	
+	// CAN Talons
+	
+	// Joysticks
+	public static final int DRIVER_JOYSTICK = 0;
+	public static final int OPERATOR_JOYSTICK = 1;
+}
