9e64bd846bbe937508f467fcff1b8c7070cd60b7
RyanSciarabba <rhsciarabba@gmail.com>
2018-01-11 17:42:17 -0600

    Revert "Deleted Recursive Code"
    This reverts commit 369c4908f903e5fe3b0cc376891c1ab77c92aa99.

---
+++ 2018-Robot/src/org/usfirst/frc/team5254/robot/OI.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team5254.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+public class OI {
+	
+	// Defining controllers
+
+	@SuppressWarnings("unused")
+	
+	public OI() {
+		
+		// Defining driver buttons
+		
+		// Defining operator buttons
+
+	}
+}
---
+++ 2018-Robot/src/org/usfirst/frc/team5254/robot/RobotMap.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team5254.robot;
+
+public class RobotMap {
+
+	// Solenoids
+	
+	// Sensors
+	
+	// Gyro
+	
+	// CAN Talons
+	
+	// Joysticks
+
+}
