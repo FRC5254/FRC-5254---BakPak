20fe322deb525361ddb9940a25291c43f331e360
Wren Martinson <42078212+TheOnePotato@users.noreply.github.com>
2018-09-08 14:22:27 -0400

    Cleaned Climber code
    to be tested later before committing to master

--- src/org/usfirst/frc/team5254/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team5254/robot/subsystems/Climber.java
@@ -10,8 +10,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Climber extends Subsystem {
 	
-	public static Victor climberMotor = new Victor(RobotMap.CLIMBER);
-	public static Victor climberMotor2 = new Victor(RobotMap.CLIMBER_2);
+	public static Victor climberMotor;
+	public static Victor climberMotor2;
+	
+	public Climber () {
+		climberMotor = new Victor(RobotMap.CLIMBER);
+		climberMotor2 = new Victor(RobotMap.CLIMBER_2);
+	}
 	
 	public void initDefaultCommand() {
 		setDefaultCommand(new ClimberFire());
@@ -20,20 +25,5 @@ public class Climber extends Subsystem {
 	public void set(double speed) {
 		climberMotor.set(speed);
 		climberMotor2.set(speed);
-	}
-	
-	public void on(boolean direction) {
-		if (direction) {
-			climberMotor.set(1.0);
-			climberMotor2.set(1.0);
-		} else {
-			climberMotor.set(-1.0);
-			climberMotor2.set(-1.0);
-		}
-	}
-	
-	public void off() {
-		climberMotor.set(0.0);
-		climberMotor2.set(0.0);
 	}
 }
